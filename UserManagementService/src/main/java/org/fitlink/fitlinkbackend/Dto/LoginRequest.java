2686e2a54319d813d6a15e5c3a09b8c373272240
randiranirman <randiranirman20011210@gmail.com>
2025-08-22 09:47:14 +0530

    fixed the records according the jwt  and spring security

--- UserManagementService/src/main/java/org/fitlink/fitlinkbackend/Dto/LoginRequest.java
+++ UserManagementService/src/main/java/org/fitlink/fitlinkbackend/Dto/LoginRequest.java
@@ -3,6 +3,6 @@ package org.fitlink.fitlinkbackend.Dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record LoginRequest(@Email  @NotBlank  String email , @NotBlank  String password) {
+public record LoginRequest(  @NotBlank(  message = "user is  required ")  String username , @NotBlank( message =  " password is required ")  String password) {
 
 }
--- UserManagementService/src/main/java/org/fitlink/fitlinkbackend/Models/AppUser.java
+++ UserManagementService/src/main/java/org/fitlink/fitlinkbackend/Models/AppUser.java
@@ -8,8 +8,13 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.Collections;
 
 @Document(collection = "app_users")
 @NoArgsConstructor
@@ -19,19 +24,59 @@ import java.time.Instant;
 
 
 
-public class AppUser {
+public class AppUser implements UserDetails {
 
 
     @Id
-    private Long Id;
+    private String  Id;
     @Indexed(  unique = true)
     private String email ;
     private String hashedPassword ;
     private String name   ;
-    private AppUserRole userRole;
+    private AppUserRole userRole= AppUserRole.CLIENT;
     private boolean enabled= true;
     private boolean fromOauth = false ;
+    private String  username ;
 
 
     private Instant createdAt = Instant.now();
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+
+        return Collections.singletonList( new SimpleGrantedAuthority("ROLE " + userRole.name()));
+
+    }
+
+    @Override
+    public String getPassword() {
+        return hashedPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return  username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return  true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
