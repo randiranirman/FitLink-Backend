ee57bd4b3c28bbf9d193acb4eed0c24d06513bcf
randiranirman <randiranirman20011210@gmail.com>
2025-09-02 22:15:37 +0530

    added pre defined meals for trainer to assign for the clients

--- TrackingService/src/main/java/org/fitlink/trackingservice/Service/MealTrackingService.java
+++ TrackingService/src/main/java/org/fitlink/trackingservice/Service/MealTrackingService.java
@@ -2,10 +2,13 @@ package org.fitlink.trackingservice.Service;
 
 
 import lombok.RequiredArgsConstructor;
-import org.fitlink.trackingservice.Client.UserClient;
+
 import org.fitlink.trackingservice.Models.FoodItem;
-import org.fitlink.trackingservice.Repository.MealTrackingRepository;
+import org.fitlink.trackingservice.Repository.FoodItemRepository;
+
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +18,26 @@ public class MealTrackingService {
 
 
     private final  NutritionService nutritionService;
+    private final FoodItemRepository foodItemRepository;
 
     public FoodItem createFoodItem (String name ,  double quantity , String unit ) {
 
+var item = nutritionService.getNutrition(name ,quantity, unit);
+foodItemRepository.save(item);
+
+     return  item ;
+
+    }
 
 
-     return nutritionService.getNutrition(name ,quantity, unit);
+
+    public List<FoodItem> getFoodItems() {
+
+         var foodItems = foodItemRepository.findAll();
+
+         return foodItems;
+
+
 
     }
 
